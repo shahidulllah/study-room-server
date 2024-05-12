83139ebfc92b624a896d8a7fea234f856b5ff424
shahidullah177 <mdshahidsumon177@gmail.com>
2024-05-12 13:00:43 +0600

    Delete function is done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -17,37 +17,45 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    //Collection
-    const assignmentCollection = client.db('assignmentDB').collection('assignment');
+    try {
+        //Collection
+        const assignmentCollection = client.db('assignmentDB').collection('assignment');
 
-   //Post assignments
-   app.post ('/assignments', async(req, res)=>{
-    const userAssignment = req.body;
-    console.log(userAssignment);
+        //Post assignments
+        app.post('/assignments', async (req, res) => {
+            const userAssignment = req.body;
+            console.log(userAssignment);
 
-     const result = await assignmentCollection.insertOne(userAssignment);
-     res.send(result)
-   })
+            const result = await assignmentCollection.insertOne(userAssignment);
+            res.send(result)
+        })
 
-   //Get assignment
-     app.get('/assignments', async (req, res) => {
-        const result = await assignmentCollection.find().toArray();
-        res.send(result);
-      })
+        //Get assignment
+        app.get('/assignments', async (req, res) => {
+            const result = await assignmentCollection.find().toArray();
+            res.send(result);
+        })
 
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    
-  }
+        //Delete document
+        app.delete('/assignments/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await assignmentCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
 }
 run().catch(console.dir);
 
@@ -55,8 +63,8 @@ run().catch(console.dir);
 //Checking
 app.get('/', (req, res) => {
     res.send('SERVER IS RUNNING SUCCESSFULLY..!')
-  })
-  
-  app.listen(port, () => {
+})
+
+app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
-  })
+})
